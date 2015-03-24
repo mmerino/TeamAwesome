53ea6b37076a07123ce93d25611386724ed2c10d
slckwd <s.lckwd@gmail.com>
2015-03-24 18:42:39 -0400

    Added functional servlet
    The servlet now works. You can test it you by making a GET request with
    /search?URL= . It won't work with the form as is but we can fix that
    easily tomorrow morning. Corrected the erroneously placed articlesearch
    library (which was the source of every single problem I was having
    today) and added jsoup library.

---
+++ ArticleSearch/src/team/awesome/ArticleSearch.java
@@ -0,0 +1,72 @@
+package team.awesome;
+
+import team.misc.ArrayOrganizer;
+import team.misc.BinarySearcher;
+import team.misc.FileReaderObject;
+import team.misc.MarkUpText;
+import team.misc.URLContentExtractor;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+//@WebServlet("/search")
+public class ArticleSearch extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public ArticleSearch() {
+        super();
+    }
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String inputUrl = request.getParameter("URL");
+		URL wordsPath = new URL("http://localhost:8080/ArticleSearch/resources/words.txt");
+		FileReaderObject words = new FileReaderObject(wordsPath);
+		ArrayList<String> wordList = words.sanitizeText(" ,\"");
+
+		URLContentExtractor urlce = new URLContentExtractor();
+
+		ArrayList<String> textArray = new ArrayList<>();
+		ArrayList<String> textWordsArray = new ArrayList<>();
+		HashMap<String, Integer> articleContains = new HashMap<>();
+		ArrayList<String> articleWords = new ArrayList<>();
+
+		URL url = new URL(inputUrl);
+		String text = urlce.read(url);
+
+		Document doc = Jsoup.parse(text, "UTF-8");
+		Elements elements = doc.select("p");
+		for (Element element : elements) {
+			textArray.add(element.text());
+		}
+
+		textWordsArray = ArrayOrganizer.createArray(textArray, ".?! ,()[]\"");
+		articleContains = BinarySearcher.search(wordList, textWordsArray);
+
+		Set<String> keySet = articleContains.keySet();
+		articleWords.addAll(keySet);
+		String markedText = MarkUpText.markUp(text, articleWords);
+		
+		request.setAttribute("marked text", markedText);
+		response.getWriter().write(markedText);
+	}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+}
